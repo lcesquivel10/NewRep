fbfef3fbe2fd6ec78336a89c26ad6e4430bed7ca
lcesquivel10 <48950395+lcesquivel10@users.noreply.github.com>
2019-07-02 11:54:42 -0500

    Add files via upload

---
+++ S5C1/S5C1Deriv.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include <cmath>
+using namespace std;
+int arr[]; // f evaluados en x
+
+float deriva(funcion, int arr[N]){
+    int evaluar = 0 ;
+    for ( int i = 0; i< N; i ++){
+        int h = N[1] - N[0] //  la distancia entre los dos puntos 
+        evaluar = funcion[i]           
+    }
+
+}
+
+
+int main(){
+  return 0 ; 
+}
